8d56f805757a445f7efe22ce14601927c4ed0072
Nttram4114 <tramtram41124@gmail.com>
2025-03-17 19:37:42 +0700

    first

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// Sale 
 document.addEventListener("DOMContentLoaded", function() {
     const closeBtn = document.querySelector(".close-btn");
     closeBtn.addEventListener("click", function() {
         document.querySelector(".top-header").style.display = "none";
     });
+});
+
+// Header
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Menu đã tải xong!");
 });
